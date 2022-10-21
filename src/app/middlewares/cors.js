5e8ddc1dfd242359e3473a248c62cc6b0e386761
nivaldoandrade <nivaldoandradef@gmail.com>
2022-10-20 23:33:36 -0300

    feat: adding 'Access-Control-Allow-Headers', 'Access-Control-Allow-Methods' and 'Access-Control-Max-Age' headers in the cors

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,5 +1,10 @@
 module.exports = (request, response, next) => {
   response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  response.setHeader('Access-Control-Allow-Headers', '*');
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Max-Age', '10');
 
   next();
 };
+
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
